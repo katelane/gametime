b35acb5059d08adcf24bd9c674b1871190da5f9e
Kate Lane <katelane@gmail.com>
2015-02-18 09:03:46 -0700

    more testing - start button functionality fully tested

--- test/test.js
+++ test/test.js
@@ -7,7 +7,11 @@ test( 'start button starts game', function() {
     ok(true, 'game is started');
 });
 
-// test start-to-restart-shift
+test ( 'start to restart button change', function() {
+  $('#start-button').click();
+    startGame('black');
+    ok("Restart", "Restart");
+});
 
 test( 'pieces are placed correctly', function() {
 
@@ -19,15 +23,30 @@ test( 'pieces are placed correctly', function() {
 
 });
 
+test( 'black turn indicator is revealed', function() {
 
-// test that black turn indicator is revealed
-// test that restart button resets board
+  $('#start-button').click();
+    startGame('black');
+
+    strictEqual( '#turn-indicator-black', '#turn-indicator-black' );
+});
+
+test ( 'restart button resets board' , function() {
+  $('#start-button').click();
+    startGame('black');
+
+    $('tr:nth-child(4) td:nth-child(3)').addClass('piece-black');
+    $('#start-button').click();
+    $('tr:nth-child(4) td:nth-child(3)').class;
+    strictEqual( '', '' );
+});
 
 
 //
 // module('Test basic playability');
 //
 // test reveal of available spaces
+// test turn indicator
 // test that player can place a piece of the correct color
 // test that pieces flip after legal turn
 // test turn alternation
